cdaf6131fe9de7a30fed938ce423488da80afdd9
aceberry924 <meobius123@gmail.com>
2020-05-28 01:37:53 +0800

    add segment buttons

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuController } from '@ionic/angular';
+import { SegmentChangeEventDetail } from '@ionic/core';
 
 import { PlacesService } from '../places.service';
 import { Place } from '../place.model';
@@ -21,6 +22,14 @@ export class DiscoverPage implements OnInit {
   ngOnInit() {
     this.loadedPlaces = this.placesService.places();
     this.listedLoadedPlaces = this.loadedPlaces.slice(1);
+  }
+
+  onOpenMenu() {
+    this.menuCtrl.toggle();
+  }
+
+  onFilterUpdate(event: CustomEvent<SegmentChangeEventDetail>) {
+    console.log(event.detail);
   }
 
 }
